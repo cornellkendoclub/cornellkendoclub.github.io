b7c0fafa64dacf8abfeec42f48163c743cbc66f1
Jeong Hyun Lee <jl2374@cornell.edu>
2019-08-22 22:37:43 -0400

    Fixed error in javascript

--- js/all.js
+++ js/all.js
@@ -2,10 +2,9 @@
 var clubinfo = " | Cornell Kendo Club";
 var path = window.location.pathname;
 var pagename = path.substring(path.lastIndexOf("/") + 1)
-if (pagename == "kendo") pagename = "home";
-else if (pagename == "eBoard") pagename = "E-Board";
-console.log(pagename);
-pagename = pagename.toUpperCase().substr(0, 1) + pagename.substring(1, pagename.indexOf("."));
+if (pagename == "index.html") pagename = "Home";
+else if (pagename == "eboard.html") pagename = "E-Board";
+pagename = pagename.toUpperCase().substr(0, 1) + pagename.substring(1);
 document.title = pagename + clubinfo;
 
 $(document).ready(function() {
